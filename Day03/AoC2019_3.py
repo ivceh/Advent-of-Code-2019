2ed60b72f769d9c61419f5066973f07acc6f5c21
Ivan Čeh <ivan.ceh1234@gmail.com>
2019-12-03 23:58:43 +0100

    Simplified Day 3

--- Day03/AoC2019_3.py
+++ Day03/AoC2019_3.py
@@ -5,41 +5,34 @@ with open("input.txt", "r") as file:
     A = [(s[0], int(s[1:])) for s in file.readline().split(',')]
     B = [(s[0], int(s[1:])) for s in file.readline().split(',')]
 
-def step(direction):
-    global pos, steps
-    steps += 1
-    if direction == 'U':
-        pos = (pos[0], pos[1] - 1)
-    elif direction == 'D':
-        pos = (pos[0], pos[1] + 1)
-    elif direction == 'L':
-        pos = (pos[0] - 1, pos[1])
-    elif direction == 'R':
-        pos = (pos[0] + 1, pos[1])
-    else:
-        raise InvalidArgument("Unknown direction!")
-
 # key: coordinates
-# value: ('A' for first wire or 'B' for second wire or 'X' for intersection,
-#         number of steps)
+# value: number of steps
 wire1 = dict()
 
 def go(wire):
-    global pos, steps
-    pos = (0, 0)
-    steps = 0
+    x = y = steps = 0
     for move in wire:
         for i in range(move[1]):
-            step(move[0])
-            yield
+            if move[0] == 'U':
+                y -= 1
+            elif move[0] == 'D':
+                y += 1
+            elif move[0] == 'L':
+                x -= 1
+            elif move[0] == 'R':
+                x += 1
+            else:
+                raise ValueError("Unknown direction!")
+            steps += 1
+            yield ((x, y), steps)
 
-for _ in go(A):
+for pos, steps in go(A):
     if not pos in wire1:
         wire1[pos] = steps
 
 inter_dists = []
 inter_steps = []
-for _ in go(B):
+for pos, steps in go(B):
     if pos in wire1:
         inter_dists.append(abs(pos[0]) + abs(pos[1]))
         inter_steps.append(wire1[pos] + steps)
