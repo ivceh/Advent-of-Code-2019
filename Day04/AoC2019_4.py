6611ebd7cf2300f767ae11437399b3d701ea4c9d
Ivan Čeh <ivan.ceh1234@gmail.com>
2019-12-04 16:04:32 +0100

    Day 4 solved

---
+++ Day04/AoC2019_4.py
@@ -0,0 +1,73 @@
+import itertools
+
+# read input
+with open("input.txt", "r") as file:
+    a, b = (int(w) for w in file.readline().split('-'))
+
+def digits_backwards(n):
+    while n > 0:
+        yield n % 10
+        n //= 10
+
+# solution 1
+def is_non_increasing(it):
+    prev = next(it)
+    for curr in it:
+        if prev < curr:
+            return False
+        prev = curr
+    return True
+
+part1 = part2 = 0
+for n in range(a, b + 1):
+    grouped = [(digit, sum(1 for _ in l))
+               for digit, l in itertools.groupby(digits_backwards(n))]
+    rule1A = any(num >= 2 for digit, num in grouped)
+    rule1B = any(num == 2 for digit, num in grouped)
+    rule2 = is_non_increasing(digit for digit, num in grouped)
+    if rule1A and rule2:
+        part1 += 1
+        if rule1B:
+            part2 += 1
+
+print("Part One", part1)
+print("Part Two", part2)
+
+#solution 2
+def first_non_decreasing_after(a):
+    digits = list(digits_backwards(a))
+    max_digit = 0
+    n = 0
+    while digits:
+        max_digit = max(max_digit, digits.pop())
+        n *= 10
+        n += max_digit
+    return n
+
+def non_decreasing_digit_range(a, b):
+    n = first_non_decreasing_after(a)
+    while n < b:
+        yield n
+        num_ending_nines = 0
+        while n % 10 == 9:
+            n //= 10
+            num_ending_nines += 1
+        n += 1
+        new_last_digit = n % 10
+        for _ in range(num_ending_nines):
+            n *= 10
+            n += new_last_digit
+
+part1 = part2 = 0
+for n in non_decreasing_digit_range(a, b):
+    groups = [sum(1 for _ in l)
+               for _, l in itertools.groupby(digits_backwards(n))]
+    rule1A = any(num >= 2 for num in groups)
+    rule1B = any(num == 2 for num in groups)
+    if rule1A:
+        part1 += 1
+        if rule1B:
+            part2 += 1
+
+print("Part One", part1)
+print("Part Two", part2)
